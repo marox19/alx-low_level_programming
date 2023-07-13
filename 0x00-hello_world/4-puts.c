80288080e2258383d021815a3a8c9d39f510bcf7
marox19 <nafex100@gmail.com>
2023-07-14 02:15:30 +0300

    kjkl

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-int main(void){
+/**
+*main - entry poit
+*return: always 0 (success)
+*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-
-return 0;
+	return (0);
 }
 
