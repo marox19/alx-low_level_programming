a78055c84134565f4350474a4ff24ae4353ac39a
marox19 <nafex100@gmail.com>
2023-07-16 19:23:15 +0300

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Description: 'print numbers of base 10'
+ *Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+	printf("%d\n", x);
+	}
+	return (0);
+}
