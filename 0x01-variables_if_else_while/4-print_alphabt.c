768cac10d19d56b1907db6735362f42f0d625a15
marox19 <nafex100@gmail.com>
2023-07-16 11:08:29 +0300

    Task 41

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -11,7 +11,12 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; ++c)
+	{
+	if (c != 'e' && c != 'q')
+	{
 	putchar(c);
+	}
+	}
 	putchar('\n');
 	return (0);
 }
