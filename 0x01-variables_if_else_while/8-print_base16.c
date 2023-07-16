1f10eec0cca4e40800e61f08c235e03bd6987a0f
marox19 <nafex100@gmail.com>
2023-07-16 23:31:39 +0300

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	int y;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	for (y = 97; y <= 102; y++)
+	{
+		putchar(y);
+	}
+	return (0);
+}
