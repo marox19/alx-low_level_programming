42cc77db2552e117ae6015aaf19d9a9fdea9c01b
marox19 <nafex100@gmail.com>
2023-07-19 00:41:34 +0300

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
