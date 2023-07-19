a50dba4fba4ddce0baff4dbd251bd65700a5cc2f
marox19 <nafex100@gmail.com>
2023-07-20 00:12:11 +0300

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+  * jack_bauer - Entry point
+  *
+  * Return: Always  (Success)
+  */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2;)
+	{
+		for (b = 0; b <= 4;)
+		{
+			for (c = 0; c <= 5;)
+			{
+				for (d = 0; d <= 9;)
+				{
+					if (a >= 2 &&b >= 4)
+						break;
+					_putchar(a + 48);
+					_putchar(b + 48);
+					_putchar(58);
+					_putchar(c + 48);
+					_putchar(d + 48);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
