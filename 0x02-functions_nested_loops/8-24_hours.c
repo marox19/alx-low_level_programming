1afa5744337951ac6b9600b935881f494ba67eea
marox19 <nafex100@gmail.com>
2023-07-20 00:19:28 +0300

    Task 8 DONE

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,13 +8,13 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 0; a <= 2;)
+	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <= 4;)
+		for (b = 0; b <= 4; b++)
 		{
-			for (c = 0; c <= 5;)
+			for (c = 0; c <= 5; c++)
 			{
-				for (d = 0; d <= 9;)
+				for (d = 0; d <= 9; d++)
 				{
 					if (a >= 2 &&b >= 4)
 						break;
