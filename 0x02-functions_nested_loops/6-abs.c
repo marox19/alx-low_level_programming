f5f707830abdcca14ad459540e696686c37a6c6b
marox19 <nafex100@gmail.com>
2023-07-19 22:55:58 +0300

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _abs - Entry point
+  * @i: intiger
+  * Return: Always 0 (Sucess)
+  */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
