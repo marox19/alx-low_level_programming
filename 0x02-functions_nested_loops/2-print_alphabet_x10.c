fde091e782fd26fcb1a001f3abfb7095b4455f17
marox19 <nafex100@gmail.com>
2023-07-19 12:54:25 +0300

    Tasnkinj

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,10 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
-
 	char x;
 
-	for (int i = 0; i < 10; i++)
-	{
-    		for (x = 'a'; x <= 'z'; x++)
-		_putchar(x);
-		_putchar('\n');
-	}
+	for (x = 'a'; x <= 'z'; x++)
+	_putchar(x);
+	_putchar('\n');
+
 }
