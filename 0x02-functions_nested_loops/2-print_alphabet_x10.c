803e05cd47247cf88800f14d6850a5f529127b88
marox19 <nafex100@gmail.com>
2023-07-19 12:42:51 +0300

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	_putchar(x);
+	_putchar('\n');
+}
