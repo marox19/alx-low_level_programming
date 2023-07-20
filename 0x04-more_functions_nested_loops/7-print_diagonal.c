bf08ce25df05b8649623b3cfa1cbc88f1441c51e
marox19 <nafex100@gmail.com>
2023-07-21 01:50:43 +0300

    Task 59

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * print_diagonal - Entry point
+  *@n: integer
+  * Return: Always 0
+  */
+void print_diagonal(int n)
+{
+        int post, space;
+
+        if (n <= 0)
+                _putchar('\n');
+        else
+        {
+                for (post = 1; post <= n ; post++)
+                {
+                        for (space = 1; space <= post; space++)
+                                _putchar(' ');
+                        _putchar(92);
+                        _putchar('\n');
+                }
+        }
+}
