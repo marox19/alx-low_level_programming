c826c57d023b7390ed2fcbf32f6bfc5038b52cd4
marox19 <nafex100@gmail.com>
2023-07-21 00:22:45 +0300

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * print_most_numbers - Entry point
+  * Return: Always 0
+  */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a == 2 || a == 4)
+			continue;
+		_putchar(a + 48);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,4 +11,5 @@ int mul(int a, int b);
 
 void print_numbers(void);
 
+void print_most_numbers(void);
 #endif /* MAIN_H */
