efba50c39ed30591e088ff1b0e75f2f6837cee60
marox19 <nafex100@gmail.com>
2023-07-28 00:27:28 +0300

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,8 +13,10 @@ void reverse_array(int *a, int n)
 	
 	int t;
 
-	for (i = 0, j = (n - 1); i < j; i++; j--)
+	for (i = 0, j = (n - 1); i < j; i++)
 	{
+		j--;
+
 		t = a[i];
 		a[i] = a[j];
 		a[j] = t;
