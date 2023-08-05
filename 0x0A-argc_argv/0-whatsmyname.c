f3eefde3d57dfebc1b2880b247117ef0b317faee
marox19 <nafex100@gmail.com>
2023-08-05 04:20:15 +0300

    Task 12345

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *@argc: char
+  *@argv: char
+  *Return: 0
+  */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *@argc: cahr
+  *@argv: chaar
+  *Return: 0
+  */
+int main(int argc, char const *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
