197f475fd27824263df4e4e7b33bd82abf33cbbe
marox19 <nafex100@gmail.com>
2023-08-11 00:16:30 +0300

    Task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+  * *string_nconcat - ENTRY point
+  *@s1: int
+  *@s2: int
+  *@n: int
+  *Return: Always 0
+  */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+
+	unsigned int i;
+
+	unsigned int j;
+
+	unsigned int s1l;
+
+	unsigned int s2l;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (s1l = 0; s1[s1l] != '\0'; s1l++)
+		;
+	for (s2l = 0; s2[s2l] != '\0'; s2l++)
+		;
+	str = malloc(s1l + n + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; j++)
+	{	str[i] = s2[j];
+		i++;
+	}
+
+	str[i] = '\0';
+	return (str);
+}
