8922c760d1b2c765f7de917917d8c52c4d4f5a1d
marox19 <nafex100@gmail.com>
2023-08-15 00:15:30 +0300

    Taskk 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+#include "main.h"
+/**
+  * struct dog - dog information
+  *@name: char
+  *@age: float
+  *@owner: char
+  *Description: dog details
+  */
+
+struct dog
+{
+	char *name;
+
+	float *age;
+
+	char *owner;
+}
+#endif /* DOG_H */
