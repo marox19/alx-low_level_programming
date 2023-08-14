d3b55657537756f272760cb3a05a96f5ac18b995
marox19 <nafex100@gmail.com>
2023-08-15 01:38:04 +0300

    Task 23

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+  * print_dog - Entry point
+  *@d: int
+  *Return: Always 0
+  */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
