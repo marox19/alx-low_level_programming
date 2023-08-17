0162a45d3b1ca664a939cbf177a5f0f4361c12da
marox19 <nafex100@gmail.com>
2023-08-17 10:29:45 +0300

    Task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,11 +4,12 @@
   *@array: char
   *@size: int
   *@action: kalb
-  *Return: funciton 
+  *Return: funciton
   */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int *end = array + size - 1;
+
 	if (array && size && action)
 		while (array <= end)
 			action(*array++);
