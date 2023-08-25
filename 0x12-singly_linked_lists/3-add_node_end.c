c81e0103024f3e6d58acb4a35df5198f4500beb8
marox19 <nafex100@gmail.com>
2023-08-26 00:04:37 +0300

    Task

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+  * add_node_end - Entry point
+  *@head: int
+  *@str: kaln
+  *Return: 0
+  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *node = *head;
+
+	if (!head || !new_node)
+		return (NULL);
+	if (str)
+	{
+		new_node->str = strdup(str);
+		if (!new_node->str)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->len = _strlen(new_node->str);
+	}
+	if (node)
+	{
+		while (node->next)
+			node = node->next;
+		node->next = new_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
+}
