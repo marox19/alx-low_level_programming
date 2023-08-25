d88014d3f146e49575f6ee357683c3407d6de1a8
marox19 <nafex100@gmail.com>
2023-08-25 23:01:23 +0300

    Tasl

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+  * _strlen - function call
+  *@s: string
+  *Return: Always
+  */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
+  * print_list - Entry point
+  *@h: pointer to first node
+  *Return: success
+  */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->str;
+		i++;
+	}
+	return (i);
+}
