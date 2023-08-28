7127c869f2031379ea1bb6023020c0bb9e8121f7
marox19 <nafex100@gmail.com>
2023-08-28 15:15:10 +0300

    Task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+  * free_listint2 - Entry point
+  *@head: st
+  *Return:
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *node, *temp;
+
+	if (!head)
+		return;
+
+	node = *head;
+
+	while (node)
+	{
+		temp = node;
+		node = node->next;
+		free(temp);
+	}
+	*head = NULL;
+}
