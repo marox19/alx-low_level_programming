0b808605c4a2f19a704759caf5eeb22e4bdc0177
marox19 <nafex100@gmail.com>
2023-08-29 15:21:33 +0300

    Tsk

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+  * pop_listint - function
+  *@head: str
+  *Return: zero
+  */
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+
+	int n;
+
+	if (!head || *head)
+		return (0);
+
+	node = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = node;
+	return (n);
+}
