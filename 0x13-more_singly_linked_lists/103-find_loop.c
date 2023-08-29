d36ef2b72995da2132f25080e810d0046f7a6f49
marox19 <nafex100@gmail.com>
2023-08-29 22:15:08 +0300

    as

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+  * *find_listint_loop - Entry
+  *@head: list
+  *Return: 0
+  */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *ptr, *end;
+
+	if (head == NULL)
+		return (NULL);
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (ptr = head; ptr != end; ptr = ptr->next)
+			if (ptr == end->next)
+				return (end->next);
+	}
+	return (NULL);
+}
